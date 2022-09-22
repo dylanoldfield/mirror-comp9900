8ee85ed36fcb8e658a2d5c92aaed2b332ccd851b
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-09-22 11:57:32 +1000

    added env

---
+++ app.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+load_dotenv()
+from flask import Flask
+import os
+import MySQLdb
+
+#db connection to planet scale
+connection = MySQLdb.connect(
+  host= os.getenv("HOST"),
+  user=os.getenv("USERNAME"),
+  passwd= os.getenv("PASSWORD"),
+  db= os.getenv("DATABASE"),
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
